6c6c4290bd6d3a688e2c507d538ed752e3d424bb
Christopher Baptista <chris@chrisbaptista.io>
2020-11-12 13:36:22 -0500

    Update ACF Fields / Modal Page

---
+++ wp-content/themes/bms-outcomes/template-parts/fc-wysiwyg.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WYSIWYG
+ */
+  $layout = get_sub_field( 'layout' );
+  $wysiwyg = get_sub_field( 'wysiwyg' );
+
+  $backgroundColor =  $layout['background_color'] ? 'bg-color--'.$layout['background_color'] : '';
+  $id = $layout['id'];
+?>
+
+<section class="content <?php echo $backgroundColor; ?>" <?php if($id){ echo 'id="'.$id.'"'; } ?>>
+  <div class="container wysiwyg__container">
+    <article>
+      <?php 
+        if( $wysiwyg['copy'] ) {
+          echo $wysiwyg['copy'];
+        }
+      ?>
+    </article>
+  </div>
+</section>
