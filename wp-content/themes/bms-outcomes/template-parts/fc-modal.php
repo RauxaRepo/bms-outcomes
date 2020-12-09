a11bd6efab5241447268396712e8e98b62a5c914
Christopher Baptista <chris@chrisbaptista.io>
2020-12-08 21:36:01 -0500

    CPM-1200 Page Modals

---
+++ wp-content/themes/bms-outcomes/template-parts/fc-modal.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Page Modal
+ */
+
+$layout = get_sub_field( 'layout' );
+$pdfFile = get_sub_field( 'pdf-file' );
+
+$backgroundColor =  $layout['background_color'] ? $layout['background_color'] : '';
+$id = $layout['id'];
+
+?>
+<section class="modal js-modal" id="<?= $id ?>" role="dialog" style="background-color: <?= $backgroundColor ?>;">
+  <div class="container modal__container">
+    <div class="modal__block">
+      <div class="modal__block modal__block--inner">
+        <div class="modal__block modal__block--close">
+          <button class="modal__button modal__button--close js-modal-close" type="button" name="button">
+            <span class="modal__span"></span>
+          </button>
+        </div>
+        <div class="modal__block modal__block--pdf js-modal-pdf" data-pdf-url="<?= $pdfFile['file'] ?>"></div>
+      </div>
+    </div>
+  </div>
+</section>
